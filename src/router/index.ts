940a34a0250e2924ffcb5db2260653a49254d17c
Dmitry <papkovdmitry@gmail.com>
2023-02-08 00:53:52 +0500

    routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { categoriesRoutes } from "@/modules/categories";
 import { homeRoutes } from "@/modules/home";
+import { preferencesRoutes } from "@/modules/preferences";
 import { projectsRoutes } from "@/modules/projects";
 import { usersRoutes } from "@/modules/users";
 import { createRouter, createWebHistory } from "vue-router";
@@ -11,6 +12,7 @@ const router = createRouter({
     ...usersRoutes,
     ...categoriesRoutes,
     ...projectsRoutes,
+    ...preferencesRoutes,
   ],
 });
 
