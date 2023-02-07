369f6e7f57dda3b1b420fda4a6839b53adc460eb
Dmitry <papkovdmitry@gmail.com>
2023-02-07 23:08:16 +0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
+      "@shared": fileURLToPath(new URL("./src/shared", import.meta.url)),
+    },
+  },
+});
