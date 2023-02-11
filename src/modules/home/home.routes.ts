0eb3b052b1fdede598517ab950620da4872fc756
Dmitry <papkovdmitry@gmail.com>
2023-02-11 09:51:17 +0500

    alias gor home route

--- src/modules/home/home.routes.ts
+++ src/modules/home/home.routes.ts
@@ -1,11 +1,12 @@
-import type { RouteRecordRaw } from "vue-router";
-import HomePage from "./HomePage.vue";
+import type { RouteRecordRaw } from "vue-router"
+import HomePage from "./HomePage.vue"
 
 export const homeRoutes: RouteRecordRaw[] = [
   {
     name: "home",
     path: "/",
+    alias: "/home",
     component: HomePage,
     meta: { layoutName: "DefaultLayout" },
   },
-];
+]
